9baa9f075a87c21f8c1f6d34728f2aca834bac72
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-03-10 15:34:08 -0500

    algo of the day

--- Arrays-Hashing/ContainsDuplicate.js
+++ Arrays-Hashing/ContainsDuplicate.js
@@ -14,20 +14,59 @@ Input: nums2 = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
 /**
+ * Hash Set - Early Exit
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/contains-duplicate/
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-  for(let right = 0; right<nums.length; right++){
-    for(let left = 0; left<right; left++){
-      const isDuplicate = nums[left] === nums[right];
-      if (isDuplicate)
-      return true;
-    }
+var containsDuplicate = (nums, numsSet = new Set()) => {
+  // going throug every value in the input array nums
+  for (const num of nums){  /*Time O(N) */
+  // we wanna know is num a duplicate , does this value already exist in our hashset?
+  // and if it does we know that our array contains duplicates. 
+  // so we dont even have to continue through rest of the array.
+  // we can immidiately return true.
+    if (numsSet.has(num))     return true;
+
+    // if it doesnt contain duplicate we are gonna add that value and 
+    // iterate through the rest of the array of nums and then loop would exit
+    numsSet.add(num);       /*Space O(N) */
   }
+  // then we can return false to indicate that we didnt find any duplicates in the array
   return false;
 
 };
 console.log(containsDuplicate(nums));
 console.log(containsDuplicate(nums1));
 console.log(containsDuplicate(nums2));
+
+
+// CONTAINS DUPLICATE : 1, 2, 3 , 1
+// First thing we look at the first number and how do we know if this is a duplicate or not we have to compare with the other.
+
+// We can also consider sorting and see how the sorting would help us.
+// 1, 1, 2, 3  
+
+// MOST EFFICINET WAY OF SOLVING IS A HASHSET
+// So we can use a HashSet but what exactly HashSet gonna do for us. It’s gonna 
+// allow us to insert elements in to the hashest in big O(1) .
+//  We can also ask our hash map does a curtain value exist? If it doesn’t exit we have to add in to the hashset
+//  so we check the others and if we found something that already in the hashset 
+//  it means we have a duplicate and we can return it true. Time O(n)
+// Space O(n) cuz we create a hashSet. Very efficient
+
+
+
+
+// var containsDuplicate = function(nums) {
+//   for(let right = 0; right<nums.length; right++){
+//     for(let left = 0; left<right; left++){
+//       const isDuplicate = nums[left] === nums[right];
+//       if (isDuplicate)
+//       return true;
+//     }
+//   }
+//   return false;
+
+// };
---
+++ Arrays-Hashing/ValidAnagram.js
@@ -0,0 +1,52 @@
+// 242. Valid Anagram
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+//  typically using all the original letters exactly once.
+
+// Example 1:
+Input: s1 = "anagram", t1 = "nagaram"
+Output: true
+
+// Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+var isAnagram = (s, t) => {
+// our first check here. For checking the length of s and t. if it's not equal length return false
+  if(s.length !=t.length){
+    return false;
+  }
+  // we need to instantiate sLetters object.This is my map between letter and number of occurrances
+  const sLetters = {};
+
+  // we are gonna iterate through s to built that map
+  for(let char of s){  /* every char in string of s  */
+    if(!sLetters[char]){  /* if it doesnt exist */
+      sLetters[char] = 1   /* we're gonna map as it occurring once */
+    }else{      /* if i encounter that letter again */
+      sLetters[char] ++ /* we are gonna increase that count */
+    } 
+  }    /* 25 to 31 these lines built the map */
+
+  // now we are gonna compare t to the map
+  for( let char of t){  /* every char in t  */
+    if(!sLetters[char]){   /* if that char doesnt exist in my map*/
+      return false    /* we can also return early and it will be false */
+    }
+    if(sLetters[char]){   /* but if it is in this map we can subtract it */
+      sLetters[char]--  /* we can subtract it */
+    }
+    if(sLetters[char] === 0){  /* and if it becomes zero */
+      delete sLetters[char] /* we are going to delete that key */
+    }
+    /* and after all the iteration of the characters if at the end everything is deleted
+    we will get an array from object.keys with a length of zero*/
+  }
+  return Object.keys(sLetters).length === 0 /* and if its zero we'll return true */
+                                            /* otherwise we return false*/
+}
+
+console.log(isAnagram(s1, t1));
+console.log(isAnagram(s, t));
