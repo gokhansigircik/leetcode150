9651e1db66862f771a152437af2619aee375acff
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-03-26 23:01:49 -0400

    solved the algo

--- Arrays-Hashing/LowestCommonAncestor.js
+++ Arrays-Hashing/LowestCommonAncestor.js
@@ -9,10 +9,24 @@ Output: 6
 // Explanation: The LCA of nodes 2 and 8 is 6.
 
 // Example 2:
-Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Input: root1 = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
 Output: 2
 // Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
 
 // Example 3:
-Input: root = [2,1], p = 2, q = 1
+Input: root2 = [2,1], p = 2, q = 1
 Output: 2
+
+var lowestCommonAncestor = function(root, p, q) {
+
+  if(p.val < root.val && q.val < root.val){
+    return lowestCommonAncestor(root.left, p, q);
+  }else if(p.val > root.val && q.val > root.val){
+    return lowestCommonAncestor(root.right, p, q);
+  }else {
+    return root;
+  }
+}
+console.log(lowestCommonAncestor(root));
+console.log(lowestCommonAncestor(root1));
+console.log(lowestCommonAncestor(root2));
