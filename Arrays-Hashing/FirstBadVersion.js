80a75352a0e554d872db4c5ba378d6dc639cc606
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-03-30 16:22:06 -0400

    commit

--- Arrays-Hashing/FirstBadVersion.js
+++ Arrays-Hashing/FirstBadVersion.js
@@ -18,3 +18,19 @@ Output: 4
 // Example 2:
 Input: n = 1, bad = 1
 Output: 1
+
+var solution = function(isBadVersion){
+  let left = 1;
+  let right = n;
+
+  while(left !== right){
+    const mid = Math.floor((left + right) / 2);
+    if(isBadVersion(mid)){
+      right = mid;
+    }else {
+      left = mid + 1;
+    }
+  }
+  return right;
+}
+console.log(solution(isBadVersion(n)));
