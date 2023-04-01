081d478352f2d9b907f32ae1fcf987c41709576e
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-03-31 21:49:38 -0400

    solved it differently

--- Arrays-Hashing/RansomNote.js
+++ Arrays-Hashing/RansomNote.js
@@ -3,36 +3,70 @@
 // Each letter in magazine can only be used once in ransomNote.
 
 // Example 1:
-Input: ransomNote = "a", magazine = "b"
-Output: false
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
 
-// Example 2:
-Input: ransomNote = "aa", magazine = "ab"
-Output: false
+// // Example 2:
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
 
-// Example 3:
-Input: ransomNote = "aa", magazine = "aab"
-Output: true
+// // Example 3:
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
 
-var canConstruct = function(ransomNote, magazine) {
-  let ransomNote = true;
-  let map = {};
-  for (let word of magazine){
-    map[word] = (map[word] || 0) + 1;
-  }
-  for (let word of ransomNote){
-    map[word] = (map[word] || 0) - 1;
-  }
-  for (let key in map){
-    if(map[key] < 0){
-      ransomNote = false;
-      break
+
+function ransom(ransomNote, magazine){
+  var ransomNoteArr = ransomNote.split(' ');
+  var magazineArr = magazine.split(' ');
+  magazineObj = {};
+
+  magazineArr.forEach(word => {
+    if(!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word]++;
+  });
+
+  var noteIsPossible = true;
+
+  ransomNoteArr.forEach(word => {
+    if(magazineObj[word]){
+      magazineObj[word]--;
+      if(magazineObj[word] < 0) noteIsPossible = false;
     }
-  }
-  if (ransomNote){
-    console.log("Yes")
-  }else{
-    console.log("No")
-  }
+    else
+    noteIsPossible = false;
+  })
+  console.log(noteIsPossible);
 }
-console.log(checkMagazine(ransomNote))
+
+var ransomNote = 'bring the money downtown'
+var magazine = 'the downtown is a cool place to visit but make sure that you bring money'
+
+ransom(ransomNote, magazine);
+
+
+
+
+
+
+// var canConstruct = function(ransomNote, magazine) {
+//   let ransomNote = true;
+//   let map = {};
+//   for (let word of magazine){
+//     map[word] = (map[word] || 0) + 1;
+//   }
+//   for (let word of ransomNote){
+//     map[word] = (map[word] || 0) - 1;
+//   }
+//   for (let key in map){
+//     if(map[key] < 0){
+//       ransomNote = false;
+//       break
+//     }
+//   }
+//   if (ransomNote){
+//     console.log("Yes")
+//   }else{
+//     console.log("No")
+//   }
+// }
+// canConstruct(ransomNote, magazine);
