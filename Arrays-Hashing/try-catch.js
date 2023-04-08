31fcdc661700bc6db373c81e3e06873253462dc4
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-04-08 09:26:01 -0400

    practice

---
+++ Arrays-Hashing/try-catch.js
@@ -0,0 +1,8 @@
+let num1 = 5;
+let num2 = "hello"; 
+try { 
+let result = num1 + num2; 
+console.log(result);
+ } catch(error) { 
+console.log("Oops, something went wrong! Error message:", error); 
+}
