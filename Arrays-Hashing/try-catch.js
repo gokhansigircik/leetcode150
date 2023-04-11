0d35afa890e291aaff8e1dc6418b74021078c8a8
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-04-10 20:49:42 -0400

    commented

--- Arrays-Hashing/try-catch.js
+++ Arrays-Hashing/try-catch.js
@@ -1,8 +1,17 @@
+// So imagine you have two things: five apples (which we'll call "num1") 
+// and a toy (which we'll call "num2"). Now, you want to add the apples and
+//  the toy together and see what you get. You might think that you'll just 
+//  get a bigger pile of apples and a toy, but actually, when you try to add 
+//  them together, you'll get an error because they're two different things 
+//  and you can't add them together.
+
 let num1 = 5;
 let num2 = "hello"; 
 try { 
 let result = num1 + num2; 
 console.log(result);
- } catch(error) { 
+} catch(error) { 
 console.log("Oops, something went wrong! Error message:", error); 
 }
+
+// console.log(typeof 5); number 
--- isPrimeNum.js
+++ isPrimeNum.js
@@ -1,4 +1,11 @@
 // prime number can only divisible by 1 and itself
+/* 
+The code if (num < 2) is commonly used in programming to check if a given number
+ num is less than 2. This is because the smallest prime number is 2, and any 
+ number less than 2 is not a prime number by definition. Therefore, 
+ if num is less than 2, we can immediately conclude that 
+ it is not a prime number and avoid further computation.
+*/
 
 function isPrime(num){  // first step is to find if the num less the 2 
   if( num < 2 ){
